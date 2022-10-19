ac3566e93fb301ccac20320c9aff97db56974571
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-19 11:35:23 +0800

    added post mapping

--- src/main/java/com/todo/ToDo/service/controllers/TodoController.java
+++ src/main/java/com/todo/ToDo/service/controllers/TodoController.java
@@ -1,9 +1,12 @@
 package com.todo.ToDo.service.controllers;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todo.ToDo.data.entities.Todo;
@@ -21,5 +24,13 @@ public class TodoController {
 		@GetMapping("/todo")
 		List<Todo> all(){
 			return repository.findAll();
+		}
+		
+		@CrossOrigin(origins="http://127.0.0.1:5500")
+		@PostMapping("/create-task")
+		Todo newEmployee(@RequestBody Todo newTodo) {
+			newTodo.setCreationDatetime(new Timestamp(System.currentTimeMillis()));
+			newTodo.setDone(false);
+			return repository.save(newTodo);
 		}
 }
