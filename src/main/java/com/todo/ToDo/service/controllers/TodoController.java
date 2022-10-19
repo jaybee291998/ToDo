4fc237d738bd2707476584468c1e8a5da32316dd
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-19 14:42:55 +0800

    added api endpoints for update and delete

--- src/main/java/com/todo/ToDo/service/controllers/TodoController.java
+++ src/main/java/com/todo/ToDo/service/controllers/TodoController.java
@@ -4,9 +4,11 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +43,25 @@ public class TodoController {
 			newTodo.setDone(false);
 			return repository.save(newTodo);
 		}
+		
+		@CrossOrigin(origins="http://127.0.0.1:5500")
+		@PutMapping("/update-task/{id}")
+		Todo updateTodo(@RequestBody Todo newTodo, @PathVariable Long id) {
+			return repository.findById(id)
+					.map(todo -> {
+						todo.setBody(newTodo.getBody());
+						todo.setDone(newTodo.isDone());
+						return repository.save(todo);
+					})
+					.orElseThrow(() -> new TodoNotFoundException(id));
+		}
+		
+		
+		@CrossOrigin(origins="http://127.0.0.1:5500")
+		@DeleteMapping("/delete-todo/{id}")
+		void deleteTodo(@PathVariable Long id) {
+			repository.deleteById(id);
+		}
+		
+		
 }
