ce22d39d2a520b5571cfe1413c68356c2c1831c2
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-10-19 13:27:04 +0800

    added endpoint to access specified todo

--- src/main/java/com/todo/ToDo/service/controllers/TodoController.java
+++ src/main/java/com/todo/ToDo/service/controllers/TodoController.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todo.ToDo.data.entities.Todo;
 import com.todo.ToDo.data.repositories.TodoRepository;
+import com.todo.ToDo.service.controllers.exceptions.TodoNotFoundException;
 @RestController
 public class TodoController {
 	
@@ -24,6 +26,12 @@ public class TodoController {
 		@GetMapping("/todo")
 		List<Todo> all(){
 			return repository.findAll();
+		}
+		
+		@CrossOrigin(origins="http://127.0.0.1:5500")
+		@GetMapping("/get-todo/{id}")
+		Todo getTodo(@PathVariable Long id) {
+			return repository.findById(id).orElseThrow(() -> new TodoNotFoundException(id));
 		}
 		
 		@CrossOrigin(origins="http://127.0.0.1:5500")
--- src/main/java/com/todo/ToDo/service/controllers/exceptions/TodoNotFoundException.java
+++ src/main/java/com/todo/ToDo/service/controllers/exceptions/TodoNotFoundException.java
@@ -7,7 +7,7 @@ public class TodoNotFoundException extends RuntimeException{
 	 */
 	private static final long serialVersionUID = -4285314166974798934L;
 	
-	TodoNotFoundException(long id){
+	public TodoNotFoundException(long id){
 		super("Could not find task with id " + id);
 	}
 }
